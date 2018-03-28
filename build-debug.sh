6e6c9a18ffb0f91155244e4b6abf7fb91f513961
Jonathan Campbell <jonathan@castus.tv>
2018-03-27 17:26:07 -0700

    more

--- build-debug.sh
+++ build-debug.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd bochs || exit 1
-./configure --prefix=/usr --enable-x86-64 --enable-smp --enable-cpu-level=6 --enable-long-phy-address --enable-large-ramfile --enable-configurable-msrs --enable-debugger --enable-disasm --enable-debugger-gui --enable-all-optimizations --enable-readline --enable-fpu --enable-vmx=2 --enable-svm --enable-alignment-check --enable-monitor-mwait --enable-perfmon --enable-memtype --enable-avx --enable-evex --enable-pci --enable-usb --enable-usb-ohci --enable-usb-ehci --enable-usb-xhci --enable-ne2000 --enable-pnic --enable-e1000 --enable-clgd54xx --enable-voodoo --enable-cdrom --enable-sb16 --enable-es1370 --enable-gameport --enable-busmouse --with-sdl2 || exit 1
+./configure --prefix=/usr --enable-x86-64 --enable-smp --enable-cpu-level=6 --enable-long-phy-address --enable-large-ramfile --enable-configurable-msrs --enable-debugger --enable-disasm --enable-debugger-gui --enable-all-optimizations --enable-readline --enable-fpu --enable-vmx=2 --enable-svm --enable-alignment-check --enable-monitor-mwait --enable-perfmon --enable-memtype --enable-avx --enable-evex --enable-pci --enable-usb --enable-usb-ohci --enable-usb-ehci --enable-usb-xhci --enable-ne2000 --enable-pnic --enable-e1000 --enable-clgd54xx --enable-voodoo --enable-cdrom --enable-sb16 --enable-es1370 --enable-gameport --enable-busmouse || exit 1
+make -j5 || exit 1
 
